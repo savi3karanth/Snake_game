f5e2ea98f379511e3aa290891c1e3db1f01ffb0e
savi3karanth <Karanth.savi3@gmail.com>
2025-02-26 19:18:58 -0500

    added the reset function

--- snake.py
+++ snake.py
@@ -6,7 +6,6 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
-
 class Snake:
 
     def __init__(self):
@@ -34,6 +33,13 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
 
     def up(self):
         if self.head.heading() != DOWN:
